536006d0fe4a04679e39ea0116013515727fbb3a
Maria <r-maria-n@mail.ru>
2021-02-19 15:01:36 +0300

    added implementation

--- algorithms/WagnerFischerAlgorithm.py
+++ algorithms/WagnerFischerAlgorithm.py
@@ -5,7 +5,7 @@ import numpy as np
 
 
 def load_values():
-    return ['some', 'other', 'sme']
+    return ['кто']
 
 
 def count_distances(value, strings):
@@ -16,10 +16,28 @@ def count_distances(value, strings):
 
 
 def count_distance(a, b):
-    # matrix = np.empty((len(a), len(b)))
-    return 0
+    m = len(a)
+    n = len(b)
+    d = np.empty((m + 1, n + 1))
+    # Расстояние от источника до пустой строки цели вычисляется удалением всех символов
+    for i in range(1, m):
+        d[i, 0] = i
+    # Расстояние от цели до пустого источника вычисляется добавлением всех символов
+    for j in range(1, n):
+        d[0, j] = j
+    for j in range(1, n):
+        for i in range(1, m):
+            if a[i] == b[j]:
+                substitution_cost = 0
+            else:
+                substitution_cost = 1
+
+            d[i, j] = min(d[i - 1, j] + 1,  # deletion
+                          d[i, j - 1] + 1,  # insertion
+                          d[i - 1, j - 1] + substitution_cost)  # substitution
+    return d[m - 1, n - 1]
 
 
 if __name__ == '__main__':
-    inp = 'some'
+    inp = 'кот'
     print(count_distances(inp, load_values()))
